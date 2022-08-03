8ca501e7710cc8f3e1a20313d1fba926e895af19
deepesh186 <deepeshagrawal93@gmail.com>
2022-08-03 11:21:58 +0530

    Sort.java

--- src/str/Sort.java
+++ src/str/Sort.java
@@ -7,6 +7,7 @@ public class Sort {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("hi deepesh");
 		// TODO Auto-generated method stub
 		int[]  l={3, 7, 2, 3, 7, 5, 5, 1};
 	
