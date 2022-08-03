457d4caba29871bdafa04e7eea7b695136e1b9cd
deepesh186 <deepeshagrawal93@gmail.com>
2022-08-03 11:05:43 +0530

    Sort.java

--- src/str/Sort.java
+++ src/str/Sort.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class Sort {
 
 	public static void main(String[] args) {
+		System.out.println("hi");
 		// TODO Auto-generated method stub
 		int[]  l={3, 7, 2, 3, 7, 5, 5, 1};
 	
